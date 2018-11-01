faa9db2a5f58cab603d7396752d211f9022ea206
Nathan Vogel <nathan@nathanvogel.com>
2018-11-01 09:59:02 -0700

    :lipstick: Basic CSS animations

--- src/dom.js
+++ src/dom.js
@@ -1,3 +1,61 @@
-function initDivs() {}
+var dataset = [
+  {
+    name: "DEFCON",
+    type: "event",
+    timestamp: 815183068,
+    year: 1995,
+    description: "uohwefiuwhfu"
+  },
+  {
+    name: "BANK OF AMERICA",
+    type: "hack",
+    timestamp: 815183068,
+    year: 1995,
+    description: "uohwefiuwhfu"
+  }
+];
 
-initDivs();
+function w() {
+  return window.innerWidth;
+}
+function h() {
+  return window.innerHeight;
+}
+var blockDistance = w() / 2;
+var container;
+
+function initBlocks() {
+  container = document.getElementById("container");
+
+  prepareDataset();
+  for (let i = 0; i < dataset.length; i++) {
+    createBlock(dataset[i], i);
+  }
+}
+
+function prepareDataset() {
+  for (let i = 0; i < dataset.length; i++) {
+    dataset[i].index = i;
+    dataset[i].id = "event-" + i;
+  }
+}
+
+function createBlock(data) {
+  var eventBlock = document.createElement("div");
+  eventBlock.id = data.id;
+  eventBlock.className = "block";
+  eventBlock.style.left = blockDistance * (data.index + 1) + "px";
+  container.appendChild(eventBlock);
+
+  // Now create and append to iDiv
+  var title = document.createElement("div");
+  title.className = "block-title";
+  title.textContent = data.name;
+  eventBlock.appendChild(title);
+}
+
+function goToBlock(index) {
+  container.style.transform = "translateX(" + -index * blockDistance + "px)";
+}
+
+initBlocks();
--- src/rendering.js
+++ src/rendering.js
@@ -23,8 +23,8 @@ app.renderer.view.style.position = "absolute";
 app.renderer.view.style.display = "block";
 app.renderer.autoResize = true;
 app.renderer.resize(window.innerWidth, window.innerHeight);
-//Add the canvas that Pixi automatically created for you to the HTML document
-document.body.appendChild(app.view);
+// Add the canvas that Pixi automatically created for you to the HTML document
+document.body.insertBefore(app.view, document.getElementById("container"));
 
 // Link canvas
 let paperCanvas = document.getElementById("canvas");
